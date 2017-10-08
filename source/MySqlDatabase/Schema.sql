c496fbfc59c38c1ea84b3d6af4d3b9a172357d8f
Glen Summers <glen.summers@outlook.com>
2017-10-08 13:25:18 +0100

    add mwb gen schema

--- source/MySqlDatabase/Schema.sql
+++ source/MySqlDatabase/Schema.sql
@@ -1,29 +1,68 @@
+-- MySQL Workbench Forward Engineering
 
-DROP SCHEMA IF EXISTS demo;
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
-CREATE SCHEMA IF NOT EXISTS demo DEFAULT CHARACTER SET utf8;
+-- -----------------------------------------------------
+-- Schema demo
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `demo` ;
 
-DROP TABLE IF EXISTS demo.Data;
+-- -----------------------------------------------------
+-- Schema demo
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `demo` DEFAULT CHARACTER SET utf8 ;
+USE `demo` ;
 
-CREATE TABLE demo.Data
-(
-	`Id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-	`DataValue` VARCHAR(255),
+-- -----------------------------------------------------
+-- Table `data`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `data` ;
 
-	PRIMARY KEY (`Id`)
-);
+CREATE TABLE IF NOT EXISTS `data` (
+  `Id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `DataValue` VARCHAR(255) NULL DEFAULT NULL,
+  PRIMARY KEY (`Id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 3
+DEFAULT CHARACTER SET = utf8;
 
-DELIMITER //
-CREATE PROCEDURE demo.AddData(IN `dataValue` VARCHAR(255), OUT `id` INT)
+USE `demo` ;
+
+-- -----------------------------------------------------
+-- procedure AddData
+-- -----------------------------------------------------
+
+USE `demo`;
+DROP procedure IF EXISTS `AddData`;
+
+DELIMITER $$
+USE `demo`$$
+CREATE DEFINER=`DemoUser`@`localhost` PROCEDURE `AddData`(IN `dataValue` VARCHAR(255), OUT `id` INT)
 BEGIN
-INSERT INTO demo.Data (`DataValue`) VALUES (`dataValue`);
+INSERT INTO Data (`DataValue`) VALUES (`dataValue`);
 SET `id` = last_insert_id();
-END //
+END$$
+
 DELIMITER ;
 
-DELIMITER //
-CREATE PROCEDURE demo.Query(IN `count` INT UNSIGNED)
+-- -----------------------------------------------------
+-- procedure Query
+-- -----------------------------------------------------
+
+USE `demo`;
+DROP procedure IF EXISTS `Query`;
+
+DELIMITER $$
+USE `demo`$$
+CREATE DEFINER=`DemoUser`@`localhost` PROCEDURE `Query`(IN `count` INT UNSIGNED)
 BEGIN
-SELECT `Id`,`DataValue` from demo.Data ORDER BY `Id` DESC limit `count`;
-END //
+SELECT `Id`,`DataValue` from Data ORDER BY `Id` DESC limit `count`;
+END$$
+
 DELIMITER ;
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
